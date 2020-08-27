24f168dc1e6ee8b12f8013aa9665c54d2ebdd4c6
Blackout03 <45116222+Blackout03@users.noreply.github.com>
2020-08-27 14:37:32 +1000

    Thicc Boi Now Here
    No Texture or Texture Updates Yet

--- src/main/java/net/potatocult/spudup/registries/ItemRegistry.java
+++ src/main/java/net/potatocult/spudup/registries/ItemRegistry.java
@@ -19,12 +19,13 @@ public class ItemRegistry {
         @SubscribeEvent
         public static void RegisterItems(final RegistryEvent.Register<Item> event) {
             final Item[] items = {
-                    new Item((new Item.Properties()).group(SpudUp.GROUP).maxStackSize(64).food(Foods.POTATO).rarity(Rarity.COMMON)).setRegistryName("minecraft:potato"),
-                    new Item((new Item.Properties()).group(SpudUp.GROUP).maxStackSize(64).food(Foods.BAKED_POTATO).rarity(Rarity.COMMON)).setRegistryName("minecraft:baked_potato"),
-                    new Item((new Item.Properties()).group(SpudUp.GROUP).maxStackSize(64).food(Foods.POISONOUS_POTATO).rarity(Rarity.UNCOMMON)).setRegistryName("minecraft:poisonous_potato"),
+                new Item((new Item.Properties()).group(SpudUp.GROUP).maxStackSize(64).food(Foods.POTATO).rarity(Rarity.COMMON)).setRegistryName("minecraft:potato"),
+                new Item((new Item.Properties()).group(SpudUp.GROUP).maxStackSize(64).food(Foods.BAKED_POTATO).rarity(Rarity.COMMON)).setRegistryName("minecraft:baked_potato"),
+                new Item((new Item.Properties()).group(SpudUp.GROUP).maxStackSize(64).food(Foods.POISONOUS_POTATO).rarity(Rarity.UNCOMMON)).setRegistryName("minecraft:poisonous_potato"),
                 new Item((new Item.Properties()).group(SpudUp.GROUP).maxStackSize(64).food(Foods.UPSIDE_DOWN_BAKED_POTATO).rarity(Rarity.UNCOMMON)).setRegistryName(SpudUp.location("upside_down_baked_potato")),
-                new Item((new Item.Properties()).group(SpudUp.GROUP).maxStackSize(64).food(Foods.GOLDEN_POTATO).rarity(Rarity.RARE)).setRegistryName(SpudUp.location("golden_baked_potato")),
-                new EnchantedGoldenAppleItem((new Item.Properties()).group(SpudUp.GROUP).maxStackSize(64).food(Foods.ENCHANTED_GOLDEN_POTATO).rarity(Rarity.EPIC)).setRegistryName(SpudUp.location("enchanted_golden_baked_potato"))
+                new Item((new Item.Properties()).group(SpudUp.GROUP).maxStackSize(64).food(Foods.GOLDEN_BAKED_POTATO).rarity(Rarity.RARE)).setRegistryName(SpudUp.location("golden_baked_potato")),
+                new EnchantedGoldenAppleItem((new Item.Properties()).group(SpudUp.GROUP).maxStackSize(64).food(Foods.ENCHANTED_GOLDEN_BAKED_POTATO).rarity(Rarity.EPIC)).setRegistryName(SpudUp.location("enchanted_golden_baked_potato")),
+                new Item((new Item.Properties()).group(SpudUp.GROUP).maxStackSize(64).food(Foods.THICC_BAKED_POTATO).rarity(Rarity.EPIC)).setRegistryName(SpudUp.location("thicc_baked_potato"))
             };
 
             for (final Item item: items) {
